87ff04e72ad2564279f727fbe4c773c617fcf19d
João Pedro (Foguinho) <62312787+joaofoguin@users.noreply.github.com>
2026-09-20 16:00:48 -0300

    Ajustar teste inicial do ExplorerWidget

--- tests/test_explorer_widget.py
+++ tests/test_explorer_widget.py
@@ -17,7 +17,7 @@ def test_explorer_widget_initializes_with_details_view():
     widget = ExplorerWidget([".mp3", ".flac"])
 
     assert widget.file_stack.currentWidget() is widget.file_tree
-    assert widget.file_list.viewMode().name == "IconMode" or widget.file_stack.currentWidget() is widget.file_tree
+    assert widget.file_stack.currentWidget() is widget.file_tree
 
 
 def test_explorer_widget_changes_display_modes():
