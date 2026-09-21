f80adb04cf49e203f580258d6b9fd83ce4428ed7
Foguinho <joaopedrosrlima@hotmail.com>
2026-09-21 11:48:19 -0300

    fix: import line break fix

--- app/main.py
+++ app/main.py
@@ -36,7 +36,8 @@ from widgets.queue_widget import QueueWidget
 from widgets.player_widget import PlayerWidget
 from widgets.explorer_widget import ExplorerWidget
 from widgets.main_menu import MainMenu
-from widgets.main_content_widget import MainContentWidget\nfrom core.branding import APP_DISPLAY_NAME, load_branding
+from widgets.main_content_widget import MainContentWidget
+from core.branding import APP_DISPLAY_NAME, load_branding
 
 
 class MainWindow(QMainWindow):
